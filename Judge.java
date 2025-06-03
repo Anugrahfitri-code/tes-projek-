c8a04056456e50baa2af917b132a1cc5359d85f3
Ira Fitrinovanda <irafitrinovanda@gmail.com>
2025-06-03 11:13:00 +0800

    Update Judge.java

--- Judge.java
+++ Judge.java
@@ -18,4 +18,12 @@ public class Judge extends EventUser {
     public String getRole() {
         return "Judge";
     }
+    System.out.println("Loading content for user: " + user.getUsername() + " (Role: " + user.getRole() + ")");
+if (user instanceof Participant) {
+    System.out.println("User is a Participant. Building participant UI.");
+} else if (user instanceof Judge) {
+    System.out.println("User is a Judge. Building judge UI.");
+} else {
+    System.out.println("User role is unrecognized.");
+}
 } 
